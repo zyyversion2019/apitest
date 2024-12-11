e277746a9df703fec5e9be8a29d5305082aff345
zyy2019@126.com <zyy2019test>
2024-12-11 17:04:00 +0800

    all

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import os
 
 import pytest
 
+# 增加日志
+
 if __name__=="__main__":
     pytest.main(["-vs", "./testcases/test_runner.py", "--alluredir", "./report/json_report", "--clean-alluredir"])
     os.system("allure generate ./report/json_report -o ./report/html_report --clean")
